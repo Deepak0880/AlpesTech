3af320f29fa14a941502a6830ec64aa7742c9050
Deepak <158290849+Deepak0880@users.noreply.github.com>
2025-10-01 23:47:34 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,20 @@
+// vite.config.ts
 import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import react from '@vitejs/plugin-react-swc';
 import path from 'path';
 
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src'), // '@' now points to 'src'
+      '@': path.resolve(__dirname, './src'), // '@' points to 'src'
+    },
+  },
+  server: {
+    host: '0.0.0.0',
+    port: 5173, // optional, default Vite port
+    proxy: {
+      '/api': 'https://alpestech-backend.onrender.com', // frontend API proxy
     },
   },
 });
